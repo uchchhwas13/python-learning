6f71c0a307eff66f295968cf62f4c19bd91b4771
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-17 10:49:29 +0600

    basic code

--- 01_Basics/hello_world.py
+++ 01_Basics/hello_world.py
@@ -1,6 +1,23 @@
 print("Hello world in python")
 
+
 def hello(n):
     print(n)
 
+
 hello("Lemon tea")
+x = 1
+
+temparature = 35
+if temparature > 30:
+    print("it's warm")
+    print("Drink water")
+print("Done")
+
+successful = False
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+print("Attempted 3 times")
