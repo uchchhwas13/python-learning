1491deb39df44a4d74cf8d49d870f8f0f808e833
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-18 09:23:40 +0600

    defining a simple route using fastAPI and installing uvicorn

--- UV-demo/test-project/main.py
+++ UV-demo/test-project/main.py
@@ -1,5 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
 def main():
     print("Hello from test-project!")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from test-project"}
 
 
 if __name__ == "__main__":
